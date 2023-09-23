c3df588b4e63e746f5a26e35fa6af4846c5c2398
carolbalbs <carolbalbinos@hotmail.com>
2023-09-23 19:01:08 -0300

    membros
    primeira versao da sessao de membros

--- js/index.js
+++ js/index.js
@@ -1,4 +1,19 @@
-var typeEffect = new Typed(".multiText",{
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    slidesPerGroup: 3,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+  var typeEffect = new Typed(".multiText",{
     strings:["coder","developer","engineer"],
     loop: true,
     typeSpeed:100,
