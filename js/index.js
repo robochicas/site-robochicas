4d4ba0f8c6ba8f64bb48ab11608a44770f618360
Leticia Oliveira <leticiasilvaoliveira14@gmail.com>
2024-02-23 23:02:20 -0300

    slides about

--- js/index.js
+++ js/index.js
@@ -59,3 +59,21 @@ const linkDoVideo = video.src;
         alternarModal();
         video.setAttribute("src","");
     });
+
+//slides
+let count = 1;
+document.getElementById("radio1").checked = true;
+
+setInterval( function(){
+    nextImage();
+}, 2000)
+
+function nextImage(){
+    count++;
+    if(count>4){
+        count = 1;
+    }
+
+    document.getElementById("radio"+count).checked = true;
+
+}
